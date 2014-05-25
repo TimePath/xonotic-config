fa9d3cdb88f4dbf73e54b52e8497324eaff2010a
TimePath <andrew.hardaker1995@gmail.com>
2014-05-25 18:36:47 +1000

    Initial checkin

---
+++ cvarlist.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+import re
+# log_file cvarlist
+# cvarlist
+# log_file ""
+with open('cvarlist.log', 'r') as f:
+	l = 1
+	for line in f.readlines():
+		l += 1
+		if not ' is ' in line:
+			continue
+		parts = line.split(' is ', maxsplit=1)
+		var = re.sub('\^[0-9]', '', parts[0])
+		rest = parts[1]
+		tokens = rest.split('"')
+		if len(tokens) > 3:
+			current = tokens[1]
+			default = tokens[3]
+			if default != current:
+			#print(var + ' "' + default + '"')
+				print(var + ' "' + current + '"')
+		else:
+			print(str(l) + ":" + str(tokens))
+		
